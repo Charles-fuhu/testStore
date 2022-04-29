31954d8f087e939aa44f80108af3ec428d7fc968
侯明君 <houmingjun2025@163.com>
2022-04-29 22:50:32 +0800

    start

---
+++ tailwind-react/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '@/widgets/Header'
+function Index() {
+  return (
+    <div className='container mx-auto p-4'>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default Index
